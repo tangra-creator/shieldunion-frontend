5767c397c6603038af2a8ee3b7a5649a523a077a
Tangra <you@example.com>
2025-06-09 00:46:25 +0100

    Add Register, SubmitCase, and Dashboard components

---
+++ src/Register.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Register() {
+  return <h2>This is the Register Page</h2>;
+}
+
+export default Register;
---
+++ src/SubmitCase.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SubmitCase() {
+  return <h2>This is the Submit Case Page</h2>;
+}
+
+export default SubmitCase;
