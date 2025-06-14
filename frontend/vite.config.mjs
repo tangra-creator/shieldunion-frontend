bd2744f731bf19decfae916bd78e421fe8b5b800
Tangra <you@example.com>
2025-06-14 01:20:33 +0100

    Fix blank page on Netlify by setting base path

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: './', // ✅ This fixes blank page issues on Netlify
+  plugins: [react()],
 });
