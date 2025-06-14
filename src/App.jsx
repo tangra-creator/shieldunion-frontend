ede7f2a7a8f6692d4983937de257615665b14ec8
Tangra <you@example.com>
2025-06-15 00:40:45 +0100

    Fix Router import and cleanup duplicate index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 // Page imports
 import Home from './pages/Home';
