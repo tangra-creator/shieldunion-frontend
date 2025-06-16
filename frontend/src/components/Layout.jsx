f4ef4040dcf9688dc87cd0dd8c5d8ec4f5b9419b
Tangra <you@example.com>
2025-06-16 12:14:47 +0100

    Cleaned public navbar to hide member-only links

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,13 +1,11 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import MemberNavbar from "./MemberNavbar";
 
 const Layout = () => {
   const location = useLocation();
 
-  // Define routes that should use the MemberNavbar
   const memberPaths = ["/member", "/vault", "/dao", "/infotrade", "/founder"];
-
   const isMemberRoute = memberPaths.some((path) =>
     location.pathname.startsWith(path)
   );
@@ -15,6 +13,11 @@ const Layout = () => {
   return (
     <>
       {isMemberRoute ? <MemberNavbar /> : <Navbar />}
+      
+      {/* This renders the page content! */}
+      <main className="p-4">
+        <Outlet />
+      </main>
     </>
   );
 };
